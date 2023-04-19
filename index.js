7d10c83aec73ee15bb8e90e594819ab28cad78d0
AntonLovkin <anton28111987@gmail.com>
2023-04-19 15:26:08 +0300

    complite styles for mobile

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// -----------menu------------
 const menuBtn = document.getElementById('menu-btn');
 const closeBtn = document.getElementById('close-btn');
 const sideBar = document.querySelector('.menu');
@@ -10,3 +11,27 @@ menuBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     sideBar.classList.toggle('visible')
 });
+
+
+// -------------timer------------
+const timer = document.getElementById('timer');
+let minutes = 10;
+let seconds = 0;
+
+function updateTimer() {
+  if (seconds === 0) {
+    if (minutes === 0) {
+      clearInterval(interval);
+      timer.innerHTML = "Time's up!";
+      return;
+    } else {
+      minutes--;
+      seconds = 59;
+    }
+  } else {
+    seconds--;
+  }
+  timer.innerHTML = (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+}
+
+const interval = setInterval(updateTimer, 1000);
